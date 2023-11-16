c688445ee9e9b8ffa6a42d367bbbfb1363a26365
Kewth <Kewth.K.D@outlook.com>
2023-11-16 20:16:09 +0800

    更新统计

--- src/memory/stats.ts
+++ src/memory/stats.ts
@@ -6,26 +6,30 @@ const PACK_LIM = 12
 const ROOM_PACK = 60 // 3000 tick 存储一次
 
 declare global {
-    interface RoomStat {
-        time: number
-        storageEnergy: number
-        RCLprogress: number
-        wallHits: number
-    }
-    interface RoomMemory {
-        nowStat?: RoomStat
-        lastStat?: RoomStat
-    }
+    // interface RoomStat {
+    //     time: number
+    //     storageEnergy: number
+    //     RCLprogress: number
+    //     wallHits: number
+    // }
+    // interface RoomMemory {
+    //     nowStat?: RoomStat
+    //     lastStat?: RoomStat
+    // }
     interface PackStat {
         cpu: number
         spawn: number
     }
     interface Memory {
         stats: {
-            time: number
+            gameTick: number
             cpu: number
-            storageEnergy: number
-            RCLprogress: number
+            credits: number
+            creditsIfSold: number
+            // storageEnergy: number
+            // RCLprogress: number
+            GCLprogressPercent: number
+            GPLprogressPercent: number
             spawnCost: number
             [otherKey: string]: number
             // R: { [roomName: string]: RoomStatGlobal }
@@ -52,29 +56,40 @@ export function addStat_spawn(b: BodyPartConstant[]) {
 }
 
 function roomStat(room: Room) {
+    const terminalEnergy = + ToN(room.terminal?.store[RESOURCE_ENERGY])
+    const terminalAll = + ToN(room.terminal?.store.getUsedCapacity())
     const storageEnergy = ToN(room.storage?.store[RESOURCE_ENERGY])
+    const storageAll = ToN(room.storage?.store.getUsedCapacity())
     const RCLprogress = ToN(room.controller?.progress)
     const wallHits = _.sum(room.walls(), obj => obj.hits) + _.sum(room.myRamparts(), obj => obj.hits)
-    // 输出
-    const stat = room.memory.lastStat || room.memory.nowStat
-    if (stat) {
-        const interval = Game.time - stat.time
-        Memory.stats[`${room.name}_storageEnergyPerTick`] = (storageEnergy - stat.storageEnergy) / interval
-        Memory.stats[`${room.name}_RCLprogressPerTick`] = RCLprogress < stat.RCLprogress
-            ? RCLprogress / interval
-            : (RCLprogress - stat.RCLprogress) / interval
-        Memory.stats[`${room.name}_wallHitsPerTick`] = (wallHits - stat.wallHits) / 100 / interval
-    }
-    // 存储
-    if (Game.time % (ROOM_PACK * PACK_TICK) <= 0) {
-        room.memory.lastStat = room.memory.nowStat
-        room.memory.nowStat = {
-            time: Game.time,
-            storageEnergy: storageEnergy,
-            RCLprogress: RCLprogress,
-            wallHits: wallHits,
-        }
-    }
+    const avgWallHits = wallHits / (room.walls().length + room.myRamparts.length)
+    Memory.stats[`${room.name}_terminalEnergy`] = terminalEnergy
+    Memory.stats[`${room.name}_terminalAll`] = terminalAll
+    Memory.stats[`${room.name}_storageEnergy`] = storageEnergy
+    Memory.stats[`${room.name}_storageAll`] = storageAll
+    Memory.stats[`${room.name}_RCLprogress`] = RCLprogress
+    Memory.stats[`${room.name}_wallHits`] = wallHits
+    Memory.stats[`${room.name}_avgWallHits`] = avgWallHits
+    // // 输出
+    // const stat = room.memory.lastStat || room.memory.nowStat
+    // if (stat) {
+    //     const interval = Game.time - stat.time
+    //     Memory.stats[`${room.name}_storageEnergyPerTick`] = (storageEnergy - stat.storageEnergy) / interval
+    //     Memory.stats[`${room.name}_RCLprogressPerTick`] = RCLprogress < stat.RCLprogress
+    //         ? RCLprogress / interval
+    //         : (RCLprogress - stat.RCLprogress) / interval
+    //     Memory.stats[`${room.name}_wallHitsPerTick`] = (wallHits - stat.wallHits) / 100 / interval
+    // }
+    // // 存储
+    // if (Game.time % (ROOM_PACK * PACK_TICK) <= 0) {
+    //     room.memory.lastStat = room.memory.nowStat
+    //     room.memory.nowStat = {
+    //         time: Game.time,
+    //         storageEnergy: storageEnergy,
+    //         RCLprogress: RCLprogress,
+    //         wallHits: wallHits,
+    //     }
+    // }
 }
 
 function globalStat() {
@@ -83,21 +98,15 @@ function globalStat() {
     Memory.tempPack = emptyPack()
     if (Memory.packList.length > PACK_LIM)
         Memory.packList.shift()
-    // 统计
-    let storageEnergy = 0
-    let RCLprogress = 0
-    for (const name in Game.rooms) {
-        const room = Game.rooms[name]
-        const ctrl = room.myController()
-        if (ctrl) {
-            RCLprogress += ctrl.progress
-            if (room.storage) storageEnergy += room.storage.store.getUsedCapacity(RESOURCE_ENERGY)
-        }
-    }
+    const creditsSold = _.sum(Object.values(Game.market.orders)
+        .filter(o => o.type == ORDER_SELL)
+        .map(o => o.remainingAmount * o.price))
     Memory.stats = {
-        time: Game.time,
-        storageEnergy: storageEnergy,
-        RCLprogress: RCLprogress,
+        gameTick: Game.time,
+        credits: Game.market.credits,
+        creditsIfSold: Game.market.credits + creditsSold,
+        GCLprogressPercent: Game.gcl.progress * 100 / Game.gcl.progressTotal,
+        GPLprogressPercent: Game.gpl.progress * 100 / Game.gpl.progressTotal,
         cpu: _.sum(Memory.packList, p => p.cpu) / (PACK_TICK * Memory.packList.length),
         spawnCost: _.sum(Memory.packList, p => p.spawn) / (PACK_TICK * Memory.packList.length),
     }
